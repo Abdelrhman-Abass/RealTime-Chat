1ab93f5210f7f3c1abc73a35313743b37cc38d98
Abdelrahman_Abass <abdoabass400@gmail.com>
2024-02-09 16:50:58 +0200

    initialize base setup & button ui component

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,17 @@
+import Button from '@/components/ui/Button';
+import { FC } from 'react'
+
+interface pageProps {
+  
+}
+
+const page: FC<pageProps> = ({}) => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="bg-black text-white underline text-2xl">Hello To Dashboard</div>
+      <Button />
+    </main>
+  );
+}
+
+export default page
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="bg-black text-white underline text-2xl">Hello There</div>
+    </main>
+  );
+}
