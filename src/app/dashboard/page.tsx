3e0202a7c0d125904aa9373c020e56617c3ed5ae
Abdelrahman_Abass <abdoabass400@gmail.com>
2024-02-09 17:34:36 +0200

    add db installation

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,7 +5,7 @@ interface pageProps {
   
 }
 
-const page: FC<pageProps> = ({}) => {
+const  page: FC<pageProps> = ({}) => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="bg-black text-white underline text-2xl">Hello To Dashboard</div>
